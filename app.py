4664618f75528d50a532f2f93fef3b0124ff1f92
algonacci <algonacci1@gmail.com>
2022-08-27 15:18:55 +0700

    learnt about API and Resource

--- app.py
+++ app.py
@@ -1,1 +1,21 @@
-__init__
+from flask import Flask
+from flask_restful import Api, Resource
+
+app = Flask(__name__)
+api = Api(app)
+
+items = []
+
+class Item(Resource):    
+    def get(self, name):
+        return {'student_name': name}
+
+    def post(self, name):
+        items.append(name)
+        return {'student_name': name}
+
+api.prefix = '/api'
+api.add_resource(Item, '/item/<string:name>')
+
+if __name__ == '__main__':
+    app.run(debug=True)
